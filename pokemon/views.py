063e58a47e189728405d421bbb8ada5883dcc122
Mirai Akagawa <mirai418@me.com>
2014-03-31 16:23:18 -0400

    voting

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from pokemon.models import *
 from django.shortcuts import *
 # Create your views here.
@@ -10,7 +10,6 @@ def home(request):
 		context['all_pokemon'] = p
 		return render(request, 'index.html', context)
 	if (request.method == "POST"):
-		print request.POST
 		try:
 			name = request.POST["name"]
 			link = request.POST["link"]
@@ -23,4 +22,18 @@ def home(request):
 		context['all_pokemon'] = p
 		return render(request, 'index.html', context)
 
-
+def vote(request, pk):
+	print pk
+	if (request.method == "POST"):
+		try:
+			p = Pokemon.objects.get(pk=pk)
+			vote = request.POST["vote"]
+			cur = p.vote
+			print cur
+			p.vote = cur + int(vote)
+			p.save()
+			return redirect('/')
+		except:
+			return redirect('/')
+	else:
+		return redirect('/')
--- wheresthepokemon/urls.py
+++ wheresthepokemon/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'pokemon.views.home', name='home'),
+    url(r'^vote/(\d+)$', 'pokemon.views.vote', name='vote'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
