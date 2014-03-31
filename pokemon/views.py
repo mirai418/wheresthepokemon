f6dbef04174786369c9676d48912c613effb6e9b
Mihir Pandya <mihir.m.pandya@gmail.com>
2014-03-31 14:52:10 -0400

    Main shit

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from pokemon.models import *
+from django.shortcuts import *
 # Create your views here.
+
+def home(request):
+    if request.method == "GET":
+        p = Pokemon.objects.all()
+        context = dict()
+        context['all_pokemon'] = p
+        return render(request, 'index.html', context)
