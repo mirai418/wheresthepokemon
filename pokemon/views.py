f2e880837e58369cc063319edaf6862da2191926
Mirai Akagawa <mirai418@me.com>
2014-03-31 16:03:40 -0400

    doing forms

--- pokemon/views.py
+++ pokemon/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import *
 # Create your views here.
 
 def home(request):
-    if request.method == "GET":
-        p = Pokemon.objects.all().order_by("-vote")
-        context = dict()
-        context['all_pokemon'] = p
-        return render(request, 'index.html', context)
+	context = dict()
+	if request.method == "GET":
+		p = Pokemon.objects.all().order_by("-vote")
+		context['all_pokemon'] = p
+		return render(request, 'index.html', context)
+	if (request.method == "POST"):
+		print request.POST
+		try:
+			name = request.POST["name"]
+			link = request.POST["link"]
+		return render(request, 'index.html', context)
+
