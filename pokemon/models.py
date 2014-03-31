1391854d146651ed24a6ba6f502160f6a398fe52
Mirai Akagawa <mirai418@me.com>
2014-03-31 14:47:41 -0400

    urls and votes

--- pokemon/models.py
+++ pokemon/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Pokemon(models.Model):
   name = models.CharField(max_length=64)
   link = models.CharField(max_length=200)
+  vote = models.IntegerField(default=0)
 
   def __str__(self):
     return self.name
-
